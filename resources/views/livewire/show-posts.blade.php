33b097992b66769efef0952aa02930fd13cd666e
EduardoGRQ <eduardo.grquinonez@gmail.com>
2021-12-09 13:07:13 -0600

    Se crea componente de livewire

---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        Este es nuestro componente livewire
+    </div>
+    
+</div>
