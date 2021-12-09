a74f597c05868596df4cbe25aa8bee87d780c731
EduardoGRQ <eduardo.grquinonez@gmail.com>
2021-12-09 15:40:42 -0600

    Se crea tabla reactiva

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class ShowPosts extends Component
 {
+    public $search;
+
     public function render()
     {
-        return view('livewire.show-posts')
+       $posts = Post::where('title', 'like' , '%' . $this->search . '%')
+                    ->orWhere('content', 'like' , '%' . $this->search . '%')->get();
+
+        return view('livewire.show-posts', compact('posts'))
                 ->layout('layouts.base');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -8,6 +8,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="flex flex-col">
+            <input type="text" wire:model="search">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -16,7 +17,7 @@
                                 <tr>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Name
+                                        ID
                                     </th>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -24,11 +25,7 @@
                                     </th>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Status
-                                    </th>
-                                    <th scope="col"
-                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Role
+                                        Content
                                     </th>
                                     <th scope="col" class="relative px-6 py-3">
                                         <span class="sr-only">Edit</span>
@@ -37,40 +34,24 @@
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
                                 <tr>
+                                    @foreach ($posts as $post)
+                                        
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="flex-shrink-0 h-10 w-10">
-                                                <img class="h-10 w-10 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-                                                    alt="">
-                                            </div>
-                                            <div class="ml-4">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    Jane Cooper
-                                                </div>
-                                                <div class="text-sm text-gray-500">
-                                                    jane.cooper@example.com
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <div class="text-sm text-gray-900">{{ $post->id }}</div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                        <div class="text-sm text-gray-500">Optimization</div>
+                                        <div class="text-sm text-gray-900">{{ $post->title }}</div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <span
-                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                            Active
-                                        </span>
+                                        <div class="text-sm text-gray-900">{{ $post->content }}</div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        Admin
-                                    </td>
+
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                     </td>
                                 </tr>
+
+                                @endforeach
 
                                 <!-- More people... -->
                             </tbody>
