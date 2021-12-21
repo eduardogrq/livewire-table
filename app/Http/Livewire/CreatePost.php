80855b16a8c4f69eff17c1691740026ed4bb8734
EduardoGRQ <eduardo.grquinonez@gmail.com>
2021-12-20 22:53:09 -0600

    Commentarios en CreatePost

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -8,30 +8,37 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
-
+    // Variable que nos ayuda a abrir y cerrar nuestro modal
     public $open = false;
 
+    // Declaramos variables para nuestro formulario de creación
     public $title, $content;
 
+    // Función encargada de renderizar y actualizar cada que hay un cambio
     public function render()
     {
         return view('livewire.create-post');
     }
 
+    // Nos permite guardar un registro dentro de nuestro modal
     public function save(){
         DB::table('posts')->insert([
             'title' => $this->title,
             'content' => $this->content
         ]);
 
+        // Método que nos permite resetear los valores una vez que termina de funcionar todo en save
         $this->reset([
             'open',
             'title',
             'content'
         ]);
 
+        // Emitimos un alert que lo podemos ver en nuestro layout del que extendemos
         $this->emit('alert', 'El post se creó satisfactoriamente');
 
-        $this->emit('render');
+        // Nos permite comunicarnos con otros componentes y poder renderizar una vez que haga un cambio en este componente
+        // Con el método emiteTo nos permite que solo se ejecute este método dentro de show-posts y evitar que renderice más funciones
+        $this->emitTo('show-posts','render');
     }
 }
